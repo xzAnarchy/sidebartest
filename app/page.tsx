190b01f401594ff29573730421dbc78a7f677ace
Gabriel Mendoza <mendozagabriel726@gmail.com>
2024-10-19 13:45:06 -0400

    01

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,328 @@
-import Image from "next/image";
+'use client';
+
+import React from 'react';
+import { Badge } from './components/Badge';
+import { PackageBadges } from './components/PackageBadges';
+import { SidebarFooter } from './components/SidebarFooter';
+import { SidebarHeader } from './components/SidebarHeader';
+import { Switch } from './components/Switch';
+import { Typography } from './components/Typography';
+import {
+  Menu,
+  menuClasses,
+  MenuItem,
+  MenuItemStyles,
+  Sidebar,
+  SubMenu,
+} from './index';
+
+type Theme = 'light' | 'dark';
+
+const themes = {
+  light: {
+    sidebar: {
+      backgroundColor: '#ffffff',
+      color: '#607489',
+    },
+    menu: {
+      menuContent: '#fbfcfd',
+      icon: '#0098e5',
+      hover: {
+        backgroundColor: '#c5e4ff',
+        color: '#44596e',
+      },
+      disabled: {
+        color: '#9fb6cf',
+      },
+    },
+  },
+  dark: {
+    sidebar: {
+      backgroundColor: '#0b2948',
+      color: '#8ba1b7',
+    },
+    menu: {
+      menuContent: '#082440',
+      icon: '#59d0ff',
+      hover: {
+        backgroundColor: '#00458b',
+        color: '#b6c8d9',
+      },
+      disabled: {
+        color: '#3e5e7e',
+      },
+    },
+  },
+};
+
+// hex to rgba converter
+const hexToRgba = (hex: string, alpha: number) => {
+  const r = parseInt(hex.slice(1, 3), 16);
+  const g = parseInt(hex.slice(3, 5), 16);
+  const b = parseInt(hex.slice(5, 7), 16);
+
+  return `rgba(${r}, ${g}, ${b}, ${alpha})`;
+};
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [collapsed, setCollapsed] = React.useState(false);
+  const [toggled, setToggled] = React.useState(false);
+  const [broken, setBroken] = React.useState(false);
+  const [rtl, setRtl] = React.useState(false);
+  const [hasImage, setHasImage] = React.useState(false);
+  const [theme, setTheme] = React.useState<Theme>('light');
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  // handle on RTL change event
+  const handleRTLChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setRtl(e.target.checked);
+  };
+
+  // handle on theme change event
+  const handleThemeChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTheme(e.target.checked ? 'dark' : 'light');
+  };
+
+  // handle on image change event
+  const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setHasImage(e.target.checked);
+  };
+
+  const menuItemStyles: MenuItemStyles = {
+    root: {
+      fontSize: '13px',
+      fontWeight: 400,
+    },
+    icon: {
+      color: themes[theme].menu.icon,
+      [`&.${menuClasses.disabled}`]: {
+        color: themes[theme].menu.disabled.color,
+      },
+    },
+    SubMenuExpandIcon: {
+      color: '#b6b7b9',
+    },
+    subMenuContent: ({ level }) => ({
+      backgroundColor:
+        level === 0
+          ? hexToRgba(
+              themes[theme].menu.menuContent,
+              hasImage && !collapsed ? 0.4 : 1
+            )
+          : 'transparent',
+    }),
+    button: {
+      [`&.${menuClasses.disabled}`]: {
+        color: themes[theme].menu.disabled.color,
+      },
+      '&:hover': {
+        backgroundColor: hexToRgba(
+          themes[theme].menu.hover.backgroundColor,
+          hasImage ? 0.8 : 1
+        ),
+        color: themes[theme].menu.hover.color,
+      },
+    },
+    label: ({ open }) => ({
+      fontWeight: open ? 600 : undefined,
+    }),
+  };
+
+  return (
+    <div
+      style={{
+        display: 'flex',
+        height: '100%',
+        direction: rtl ? 'rtl' : 'ltr',
+      }}
+    >
+      <Sidebar
+        collapsed={collapsed}
+        toggled={toggled}
+        onBackdropClick={() => setToggled(false)}
+        onBreakPoint={setBroken}
+        image='https://user-images.githubusercontent.com/25878302/144499035-2911184c-76d3-4611-86e7-bc4e8ff84ff5.jpg'
+        rtl={rtl}
+        breakPoint='md'
+        backgroundColor={hexToRgba(
+          themes[theme].sidebar.backgroundColor,
+          hasImage ? 0.9 : 1
+        )}
+        rootStyles={{
+          color: themes[theme].sidebar.color,
+        }}
+      >
+        <div
+          style={{ display: 'flex', flexDirection: 'column', height: '100%' }}
+        >
+          <SidebarHeader
+            rtl={rtl}
+            style={{ marginBottom: '24px', marginTop: '16px' }}
+          />
+          <div style={{ flex: 1, marginBottom: '32px' }}>
+            <div style={{ padding: '0 24px', marginBottom: '8px' }}>
+              <Typography
+                variant='body2'
+                fontWeight={600}
+                style={{ opacity: collapsed ? 0 : 0.7, letterSpacing: '0.5px' }}
+              >
+                General
+              </Typography>
+            </div>
+            <Menu menuItemStyles={menuItemStyles}>
+              <SubMenu
+                label='Charts'
+                // icon={<BarChart />}
+                suffix={
+                  <Badge variant='danger' shape='circle'>
+                    6
+                  </Badge>
+                }
+              >
+                <MenuItem> Pie charts</MenuItem>
+                <MenuItem> Line charts</MenuItem>
+                <MenuItem> Bar charts</MenuItem>
+              </SubMenu>
+              <SubMenu
+                label='Maps'
+                // icon={<Global />}
+              >
+                <MenuItem> Google maps</MenuItem>
+                <MenuItem> Open street maps</MenuItem>
+              </SubMenu>
+              <SubMenu
+                label='Theme'
+                // icon={<InkBottle />}
+              >
+                <MenuItem> Dark</MenuItem>
+                <MenuItem> Light</MenuItem>
+              </SubMenu>
+              <SubMenu
+                label='Components'
+                // icon={<Diamond />}
+              >
+                <MenuItem> Grid</MenuItem>
+                <MenuItem> Layout</MenuItem>
+                <SubMenu label='Forms'>
+                  <MenuItem> Input</MenuItem>
+                  <MenuItem> Select</MenuItem>
+                  <SubMenu label='More'>
+                    <MenuItem> CheckBox</MenuItem>
+                    <MenuItem> Radio</MenuItem>
+                  </SubMenu>
+                </SubMenu>
+              </SubMenu>
+              <SubMenu
+                label='E-commerce'
+                // icon={<ShoppingCart />}
+              >
+                <MenuItem> Product</MenuItem>
+                <MenuItem> Orders</MenuItem>
+                <MenuItem> Credit card</MenuItem>
+              </SubMenu>
+            </Menu>
+
+            <div
+              style={{
+                padding: '0 24px',
+                marginBottom: '8px',
+                marginTop: '32px',
+              }}
+            >
+              <Typography
+                variant='body2'
+                fontWeight={600}
+                style={{ opacity: collapsed ? 0 : 0.7, letterSpacing: '0.5px' }}
+              >
+                Extra
+              </Typography>
+            </div>
+
+            <Menu menuItemStyles={menuItemStyles}>
+              <MenuItem
+                // icon={<Calendar />}
+                suffix={<Badge variant='success'>New</Badge>}
+              >
+                Calendar
+              </MenuItem>
+              <MenuItem
+              // icon={<Book />}
+              >
+                Documentation
+              </MenuItem>
+              <MenuItem
+                disabled
+                // icon={<Service />}
+              >
+                Examples
+              </MenuItem>
+            </Menu>
+          </div>
+          <SidebarFooter collapsed={collapsed} />
+        </div>
+      </Sidebar>
+
+      <main>
+        <div style={{ padding: '16px 24px', color: '#44596e' }}>
+          <div style={{ marginBottom: '16px' }}>
+            {broken && (
+              <button
+                className='sb-button'
+                onClick={() => setToggled(!toggled)}
+              >
+                Toggle
+              </button>
+            )}
+          </div>
+          <div style={{ marginBottom: '48px' }}>
+            <Typography variant='h4' fontWeight={600}>
+              React Pro Sidebar
+            </Typography>
+            <Typography variant='body2'>
+              React Pro Sidebar provides a set of components for creating high
+              level and customizable side navigation
+            </Typography>
+            <PackageBadges />
+          </div>
+
+          <div style={{ padding: '0 8px' }}>
+            <div style={{ marginBottom: 16 }}>
+              <Switch
+                id='collapse'
+                checked={collapsed}
+                onChange={() => setCollapsed(!collapsed)}
+                label='Collapse'
+              />
+            </div>
+
+            <div style={{ marginBottom: 16 }}>
+              <Switch
+                id='rtl'
+                checked={rtl}
+                onChange={handleRTLChange}
+                label='RTL'
+              />
+            </div>
+
+            <div style={{ marginBottom: 16 }}>
+              <Switch
+                id='theme'
+                checked={theme === 'dark'}
+                onChange={handleThemeChange}
+                label='Dark theme'
+              />
+            </div>
+
+            <div style={{ marginBottom: 16 }}>
+              <Switch
+                id='image'
+                checked={hasImage}
+                onChange={handleImageChange}
+                label='Image'
+              />
+            </div>
+          </div>
         </div>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
